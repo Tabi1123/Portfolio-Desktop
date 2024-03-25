0512c8c906b6c5982e7842985d22292d26e11e1a
TAYYAB AHMAD <89637714+Tabi1123@users.noreply.github.com>
2024-03-25 23:27:50 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let hamburger = document.querySelector('.hamburger');
+let menu = document.querySelector('.navbar');
+let bod = document.querySelector('.container');
+
+hamburger.addEventListener('click', function() {
+  hamburger.classList.toggle('inactive');
+  menu.classList.toggle('active');
+
+});
